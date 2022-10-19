ee21e56e1facf1caec2ac208757013c475208536
Igor Dosinchuk <igor.dfb@hotmail.es>
2022-10-19 03:44:14 -0700

    first commit

---
+++ src/main/java/com/idosinchuk/igorspringboot/IgorSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.idosinchuk.igorspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgorSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgorSpringBootApplication.class, args);
+	}
+
+}
